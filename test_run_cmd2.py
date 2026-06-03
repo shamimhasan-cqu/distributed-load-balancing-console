8a2ce8a9f77d2c676d0f5a556137377047557585
Kamrul Hasan Shamim <162402363+shamimhasan-cqu@users.noreply.github.com>
2026-06-03 15:12:55 +1000

    Delete test_run_cmd2.py

--- test_run_cmd2.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.getcwd(), 'backend'))
-
-from backend.app.services.azure_service import azure_vm_service
-
-client = azure_vm_service._get_client()
-
-status = azure_vm_service.get_vm_status('worker-vm-1')
-print(status)
